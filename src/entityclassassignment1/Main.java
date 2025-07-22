455f0dc20cd65feccc885e10aef88cdac764a9ce
Vishwa658 <HP@LAPTOP-RDRV1JMU.bbrouter>
2025-07-22 21:21:49 +0530

    this is my assignment

---
+++ src/entityclassassignment1/Main.java
@@ -0,0 +1,14 @@
+package entityclassassignment1;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(101, "Arun", "IT");
+        Student s2 = new Student(102, "Divya", "CSE");
+        Student s3 = new Student(103, "Kiran", "ECE");
+
+        System.out.println("Student Details:");
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
